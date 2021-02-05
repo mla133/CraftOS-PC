e0149a00a4deab1fc5bc0e64992a056c304256b5
Matt Allen <matthew.allen@technipfmc.com>
2021-02-05 13:48:34 -0500

    Initial commit of CraftOS-PC

---
+++ computer/0/welcome.lua
@@ -0,0 +1,7 @@
+monitor = peripheral.wrap("right")
+monitor.clear()
+monitor.setBackgroundColor(colors.white)
+monitor.setTextColor(colors.red)
+monitor.setTextScale(1)
+monitor.write("Hello World")
+
